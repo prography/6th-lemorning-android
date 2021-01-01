9b9f46182039907dc9532523ee26248237dc3d14
조연우 <wooisso@naver.com>
2021-01-02 04:27:11 +0900

    결제카드 model 작성

--- app/src/main/java/org/prography/lemorning/src/models/Card.kt
+++ app/src/main/java/org/prography/lemorning/src/models/Card.kt
@@ -3,8 +3,14 @@ package org.prography.lemorning.src.models
 import com.google.gson.annotations.SerializedName
 
 data class Card (
-    @SerializedName("bank") val bank : String,
-    @SerializedName("no") val no : String
+    @SerializedName("id") val id : Int,
+    @SerializedName("creditCardBank") val bank : String,
+    @SerializedName("cardNickname") val nickname : String,
+    @SerializedName("cardNum") val cardNum : String,
+    @SerializedName("expireYear") val expireYear : Int,
+    @SerializedName("expireMonth") val expireMonth : Int,
+    @SerializedName("birth") val birth : String, // 1997-05-15
+    @SerializedName("simplePassword") val pwd2digit: String,
 )
 
 data class CardRegisterParam(
